3744062cda22e952d155708f8bf5d2c9234f5776
Paul Rybitskyi <paul.rybitskyi@gmail.com>
2020-08-10 01:21:20 +0300

    Add files to commons-material module

---
+++ commons-material/src/main/java/com/paulrybitskyi/commons/material/utils/CardViewExtensions.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2020 Paul Rybitskyi, paul.rybitskyi.work@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.paulrybitskyi.commons.material.utils
+
+import androidx.annotation.ColorInt
+import com.google.android.material.card.MaterialCardView
+import com.paulrybitskyi.commons.ktx.toColorStateList
+
+
+fun MaterialCardView.setRippleColor(@ColorInt color: Int) {
+    rippleColor = color.toColorStateList()
+}
---
+++ commons-material/src/main/java/com/paulrybitskyi/commons/material/utils/TabLayoutExtensions.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2020 Paul Rybitskyi, paul.rybitskyi.work@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.paulrybitskyi.commons.material.utils
+
+import com.google.android.material.tabs.TabLayout
+
+
+inline fun TabLayout.addOnTabSelectedListener(
+    crossinline onTabSelected: (TabLayout.Tab) -> Unit = {},
+    crossinline onTabReselected: (TabLayout.Tab) -> Unit = {},
+    crossinline onTabUnselected: (TabLayout.Tab) -> Unit = {}
+): TabLayout.OnTabSelectedListener {
+    return object : TabLayout.OnTabSelectedListener {
+
+        override fun onTabSelected(tab: TabLayout.Tab) = onTabSelected(tab)
+        override fun onTabReselected(tab: TabLayout.Tab) = onTabReselected(tab)
+        override fun onTabUnselected(tab: TabLayout.Tab) = onTabUnselected(tab)
+
+    }
+    .also(::addOnTabSelectedListener)
+}
