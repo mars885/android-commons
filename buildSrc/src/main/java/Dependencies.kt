3926cb8c3271b749c80cedec3470608e6b84e1ff
Paul Rybitskyi <paul.rybitskyi@gmail.com>
2021-04-07 18:44:53 +0300

    Migrate to Maven Central

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2021 Paul Rybitskyi, paul.rybitskyi.work@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:Suppress("ClassName")
+
+import org.gradle.api.JavaVersion
+
+
+object appConfig {
+
+    const val compileSdkVersion = 29
+    const val targetSdkVersion = 29
+    const val minSdkVersion = 21
+    const val buildToolsVersion = "29.0.2"
+    const val applicationId = "com.paulrybitskyi.commons.sample"
+
+    val javaCompatibilityVersion = JavaVersion.VERSION_1_8
+    val kotlinCompatibilityVersion = JavaVersion.VERSION_1_8
+
+}
+
+
+object versions {
+
+    const val kotlin = "1.4.21"
+    const val gradlePlugin = "4.1.1"
+    const val gradleVersionsPlugin = "0.36.0"
+    const val dokkaPlugin = "1.4.30"
+    const val appCompatVersion = "1.2.0"
+    const val lifecycleVersion = "2.2.0"
+    const val constraintLayoutVersion = "2.0.2"
+    const val coreKtxVersion = "1.3.1"
+    const val recyclerViewVersion = "1.1.0"
+    const val materialComponentsVersion = "1.2.0"
+    const val navigationVersion = "2.3.2"
+    const val jUnitVersion = "4.13.1"
+    const val jUnitExtVersion = "1.1.1"
+    const val espressoVersion = "3.2.0"
+
+}
+
+
+object publishingConfig {
+
+    const val artifactGroupId = "com.paulrybitskyi.commons"
+    const val artifactWebsite = "https://github.com/mars885/android-commons"
+
+    const val mavenPublicationName = "release"
+
+    const val licenseName = "The Apache Software License, Version 2.0"
+    const val licenseUrl = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+    const val developerId = "mars885"
+    const val developerName = "Paul Rybitskyi"
+    const val developerEmail = "paul.rybitskyi.work@gmail.com"
+    const val siteUrl = "https://github.com/mars885/android-commons"
+    const val gitUrl = "https://github.com/mars885/android-commons.git"
+
+    const val hostRepoName = "sonatype"
+    const val hostRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+
+    // Artifact specifics
+    const val commonsCoreArtifactName = "commons-core"
+    const val commonsCoreArtifactVersion = "1.0.1"
+
+    const val commonsDeviceInfoArtifactName = "commons-device-info"
+    const val commonsDeviceInfoArtifactVersion = "1.0.0"
+
+    const val commonsKtxArtifactName = "commons-ktx"
+    const val commonsKtxArtifactVersion = "1.0.1"
+
+    const val commonsListenersArtifactName = "commons-listeners"
+    const val commonsListenersArtifactVersion = "1.0.0"
+
+    const val commonsMaterialArtifactName = "commons-material"
+    const val commonsMaterialArtifactVersion = "1.0.1"
+
+    const val commonsNavigationArtifactName = "commons-navigation"
+    const val commonsNavigationArtifactVersion = "1.0.1"
+
+    const val commonsNetworkArtifactName = "commons-network"
+    const val commonsNetworkArtifactVersion = "1.0.0"
+
+    const val commonsRecyclerViewArtifactName = "commons-recyclerview"
+    const val commonsRecyclerViewArtifactVersion = "1.0.0"
+
+    const val commonsWidgetsArtifactName = "commons-widgets"
+    const val commonsWidgetsArtifactVersion = "1.0.1"
+
+    const val commonsWindowAnimsArtifactName = "commons-window-anims"
+    const val commonsWindowAnimsArtifactVersion = "1.0.0"
+
+    var artifactName = ""
+    var artifactVersion = ""
+
+}
+
+
+object deps {
+
+    object plugins {
+
+        const val androidGradle = "com.android.tools.build:gradle:${versions.gradlePlugin}"
+        const val kotlinGradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:${versions.kotlin}"
+        const val gradleVersions = "com.github.ben-manes:gradle-versions-plugin:${versions.gradleVersionsPlugin}"
+        const val dokka = "org.jetbrains.dokka:dokka-gradle-plugin:${versions.dokkaPlugin}"
+
+    }
+
+    object local {
+
+        const val commons = ":commons"
+        const val commonsKtx = ":commons-ktx"
+        const val commonsWindowAnims = ":commons-window-anims"
+        const val commonsRecyclerView = ":commons-recyclerview"
+        const val commonsMaterial = ":commons-material"
+        const val commonsNavigation = ":commons-navigation"
+        const val commonsDeviceInfo = ":commons-device-info"
+        const val commonsNetwork = ":commons-network"
+        const val commonsWidgets = ":commons-widgets"
+        const val commonsListeners = ":commons-listeners"
+
+    }
+
+    const val stdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${versions.kotlin}"
+    const val appCompat = "androidx.appcompat:appcompat:${versions.appCompatVersion}"
+    const val lifecycleCommonJava8 = "androidx.lifecycle:lifecycle-common-java8:${versions.lifecycleVersion}"
+    const val lifecycleViewModel = "androidx.lifecycle:lifecycle-viewmodel:${versions.lifecycleVersion}"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:${versions.constraintLayoutVersion}"
+    const val coreKtx = "androidx.core:core-ktx:${versions.coreKtxVersion}"
+    const val recyclerView = "androidx.recyclerview:recyclerview:${versions.recyclerViewVersion}"
+    const val materialComponents = "com.google.android.material:material:${versions.materialComponentsVersion}"
+    const val navigation = "androidx.navigation:navigation-fragment-ktx:${versions.navigationVersion}"
+    const val jUnit = "junit:junit:${versions.jUnitVersion}"
+    const val jUnitExt = "androidx.test.ext:junit:${versions.jUnitExtVersion}"
+    const val espressoCore = "androidx.test.espresso:espresso-core:${versions.espressoVersion}"
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+rootProject.name = "AndroidCommons"
+
+include(":app")
+include(":commons")
+include(":commons-ktx")
+include(":commons-window-anims")
+include(":commons-recyclerview")
+include(":commons-material")
+include(":commons-navigation")
+include(":commons-device-info")
+include(":commons-network")
+include(":commons-widgets")
+include(":commons-listeners")
